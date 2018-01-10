aa722dd3221b9d8d9c0028a770cecbbf884f0181
PhilipFortio93 <philip.fortio@gmail.com>
2018-01-10 20:24:14 +0000

    added env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/liquidity_portal_db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : process.env.MONGODB_URI // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
